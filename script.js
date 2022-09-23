2dd8d18b11b5acceb9ad687aead33dcddf40fa53
Anupriya Jain <86339110+anupriyajain595@users.noreply.github.com>
2022-09-23 11:47:57 +0530

    connect

---
+++ script.js
@@ -0,0 +1,8 @@
+
+  //typing animation script
+  var typed = new Typed(".typing", {
+    strings: ["frontend-developer","Learner.","Explorer."],
+   typeSpeed: 100,
+   backSpeed: 60,
+   loop: true
+});
